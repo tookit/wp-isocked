7480f8054f6c391d2127af278f5a4da113a35cf0
michael.wang <wangqiangshen@gmail.com>
2024-02-24 10:40:49 +0800

    add single page template

---
+++ single.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ *
+ * @package Shiftcoder
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ */
+
+
+get_header(); ?>
+
+<main class="page-single">
+
+
+
+      <?php the_content(); ?>
+
+
+
+
+</main>
+
+<?php get_footer();
